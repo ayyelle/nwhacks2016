467813e876e89f1d8ce8c0492d6e9d923b9d9c4d
Edward Cai <edwardcai@outlook.com>
2016-02-28 03:41:34 -0800

    added washroom markers under bus tab

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
+from xml.etree.ElementTree import fromstring
+
 from flask import Flask, render_template, request,json,jsonify, redirect
 import urllib
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -22,6 +25,7 @@ def getact():
         url = "https://developers.zomato.com/api/v2.1/search?lat="+lati+"&lon="+longi+"&radius="+dist+"&apikey=40b2fe93e6a1e1e123e6f67b846a5696"
         response = urllib.urlopen(url)
         data = json.loads(response.read())
+        print data
         amount = int(data["results_shown"]);
         listOfLatLon = [];
         for i in range (0,amount):
@@ -37,22 +41,23 @@ def getact():
         return render_template('index2.html',title=name,user=user,latitude=lati,longitude=longi,listOf=listOfLatLon)
 
     elif acttype=="bus":
-        url = "http://api.translink.ca/rttiapi/v1/stops?apikey=tnQyev7bLMYr6y43Cs2e&lat="+lati+"&long="+longi+"&radius="+dist
+        url = "http://amenimaps.com/amenimapi.php?amenity=toilet&mylat="+lati+"&mylon="+longi+"&mode=json&name=rara_pirates&key=773afa6b5638d5ce24e12e9acbe30bb2"
         response = urllib.urlopen(url)
         data = json.loads(response.read())
-        amount = int(data["results_shown"]);
+        amount = len(data["markers"])
         listOfLatLon = [];
         for i in range (0,amount):
-         lat = data["restaurants"][i]["restaurant"]["location"]["latitude"]
-         lon = data["restaurants"][i]["restaurant"]["location"]["longitude"]
+         lat = data["markers"][i]["latitude"]
+         lon = data["markers"][i]["longitude"]
          coordinates = [float(lat),float(lon)]
          listOfLatLon.append(coordinates)
 
         name = "Map"
-        latitude = data["restaurants"][2]["restaurant"]["location"]["latitude"]
-        longitude = data["restaurants"][2]["restaurant"]["location"]["longitude"]
         user = {'nickname': "Anna"}
         return render_template('index2.html',title=name,user=user,latitude=lati,longitude=longi,listOf=listOfLatLon)
+
+
+
 
    # return redirect('/')
 
